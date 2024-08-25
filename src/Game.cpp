186c5086781b5a410b5f100e5396174f4d204df2
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2024-08-25 15:01:31 +0300

    Mostly testing at this point.
    Mostly testing thing in here committing just in case to have a reference to thing I tried.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,34 +1,49 @@
 #include "Game.h"
 
+#include <string>
 #include "raylib.h"
+
+#include "Graphics/SpriteImage.h"
 
 Lumireth::Game::Game()
 {
     this->mainCamera.target = {0, 0};
-    this->mainCamera.zoom = 1.f;
+    this->mainCamera.zoom = 5.f;
     this->mainCamera.offset = {0, 0};
+
+    // TODO: Only test remove
+    this->knigthSS = std::make_shared<SpriteSheet>("Resources/Idle.png", 4, 2);
+    this->knight = std::make_unique<PlayerCharacter>();
+    this->knight->AddSpriteAnimation(knigthSS.get(), "idle", 1, 2, 1, 4, 8);
+    map = LoadTexture("Resources/mapOne.png");
 }
 
 Lumireth::Game::~Game()
 {
+    // TODO: Only test remove
+    UnloadTexture(map);
 }
 
 void Lumireth::Game::Update()
 {
+    //TODO: Only test remove
+    knight->Update((float)GetMonitorWidth(0) / 2 - 350, (float)GetMonitorHeight(0) / 2 + 60, GetFrameTime());
 
+    mainCamera.target = {(float)GetMonitorWidth(0) / 2 - 400, (float)GetMonitorHeight(0) / 2 - 100};
 }
 
 void Lumireth::Game::Render()
 {
     BeginDrawing();
+    ClearBackground(BLACK);
 
-    ClearBackground(WHITE);
+        BeginMode2D(this->mainCamera);
 
-    BeginMode2D(this->mainCamera);
+            // TODO: Only test remove
+            DrawTexture(map, 0, 0, WHITE);
+            knight->Render(WHITE);
 
-        //DrawText("Test", GetMonitorWidth(0) / 2, GetMonitorHeight(0) / 2, 70, RED);
-
-    EndMode2D();
+        EndMode2D();
 
     EndDrawing();
 }
--- src/Game.h
+++ src/Game.h
@@ -1,6 +1,12 @@
 #pragma once
 
+#include <memory>
+
 #include "raylib.h"
+
+#include "Graphics/SpriteImage.h"
+#include "Graphics/SpriteAnimation.h"
+#include "Entity/PlayerCharacter.h"
 
 namespace Lumireth
 {
@@ -8,6 +14,11 @@ namespace Lumireth
     {
     private:
         Camera2D mainCamera = { 0 };
+
+        // TODO: Testing only remove
+        std::unique_ptr<Character> knight;
+        std::shared_ptr<SpriteSheet> knigthSS;
+        Texture map;
 
     public:
         Game();
