60307c1864f70e9941b00742cb14c473b153c6f1
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2025-04-17 15:27:50 +0300

    ColliderBase update

--- src/Collider/ColliderBase.cpp
+++ src/Collider/ColliderBase.cpp
@@ -1,10 +1,5 @@
 #include "ColliderBase.h"
 
-void ColliderBase::SetLocation(Vector2 location)
-{
-    this->location = location;
-}
-
 Vector2 ColliderBase::GetLocation() const
 {
     return this->location;
--- src/Collider/ColliderBase.h
+++ src/Collider/ColliderBase.h
@@ -4,13 +4,13 @@
 
 class ColliderBase
 {
-private:
+protected:
     Vector2 location;
     
 public:
 
-    void SetLocation(Vector2 location);
-    Vector2 GetLocation() const;
+    virtual void SetLocation(Vector2 location) = 0;
+    virtual Vector2 GetLocation() const;
 
     // TODO: Limit the debuiging only for debug builds.
     // Drawing the collider for easyer debuging. Development only.
