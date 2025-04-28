e061841727dc00485fbaa7b504e826392f25151f
Ivan Kukutsov <43553573+WasteOfRAM@users.noreply.github.com>
2025-04-28 12:27:18 +0300

    Update Entity.h

--- src/Entity/Entity.h
+++ src/Entity/Entity.h
@@ -10,10 +10,13 @@ namespace Lumireth
         Vector2 location{ 0, 0 };
 
     public:
-        Entity(/* args */);
+        Entity();
         ~Entity();
 
         virtual void SetLocation(Vector2 location);
         virtual Vector2 GetLocation() const;
+
+        virtual void Update(float frameTime) = 0;
+        virtual void Draw(Color tint = WHITE) = 0;
     };
 }
